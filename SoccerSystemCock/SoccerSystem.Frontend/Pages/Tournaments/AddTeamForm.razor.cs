20481c203baface592ce3cba03e1fbfe30475a10
janier2000 <jomr10@hotmail.com>
2026-05-16 16:18:41 -0500

    video 54

--- SoccerSystemCock/SoccerSystem.Frontend/Pages/Tournaments/AddTeamForm.razor.cs
+++ SoccerSystemCock/SoccerSystem.Frontend/Pages/Tournaments/AddTeamForm.razor.cs
@@ -47,7 +47,7 @@ public partial class AddTeamForm
         if (responseHttp.Error)
         {
             var message = await responseHttp.GetErrorMessageAsync();
-            Snackbar.Add(Localizer[message], Severity.Error);
+            Snackbar.Add(Localizer[message!], Severity.Error);
             return;
         }
 
