ff46e5489194afbc79e394a29ae2108f1e978d68
janier2000 <jomr10@hotmail.com>
2026-05-17 13:03:35 -0500

    video 65

--- SoccerSystemCock/SoccerSystem.Frontend/Pages/Groups/GroupCreate.razor.cs
+++ SoccerSystemCock/SoccerSystem.Frontend/Pages/Groups/GroupCreate.razor.cs
@@ -1,5 +1,50 @@
+using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using MudBlazor;
+using SoccerSystem.Frontend.Repositories;
+using SoccerSystem.Shared.DTOs;
+using SoccerSystem.Shared.Entites;
+using SoccerSystem.Shared.Resources;
+
 namespace SoccerSystem.Frontend.Pages.Groups;
 
 public partial class GroupCreate
 {
+    private GroupForm? groupForm;
+    private GroupDTO groupDTO = new() { IsActive = true };
+
+    [Inject] private IRepository Repository { get; set; } = null!;
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
+    [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
+    [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+
+    private async Task CreateAsync()
+    {
+        groupDTO.Code = "123456";
+        groupDTO.AdminId = "123456";
+        var responseHttp = await Repository.PostAsync<GroupDTO, Group>("/api/groups/full", groupDTO);
+        if (responseHttp.Error)
+        {
+            var message = await responseHttp.GetErrorMessageAsync();
+            Snackbar.Add(Localizer[message!], Severity.Error);
+            return;
+        }
+        var group = responseHttp.Response;
+
+        Return();
+        var result = await SweetAlertService.FireAsync(new SweetAlertOptions
+        {
+            Title = Localizer["Confirmation"],
+            Text = string.Format(Localizer["GroupCreated"], group!.Name, group.Code),
+            Icon = SweetAlertIcon.Info,
+        });
+    }
+
+    private void Return()
+    {
+        groupForm!.FormPostedSuccessfully = true;
+        NavigationManager.NavigateTo("/groups");
+    }
 }
